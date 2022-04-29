16554a78f743c6206b84925c6a23f376cbe0ccac
Sebastian Bolling <sebastian.bolling@bookbeat.com>
2022-04-29 13:18:47 +0200

    Initial Commit

---
+++ Shared/AsyncPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncPlayApp.swift
+//  Shared
+//
+//  Created by Sebastian Bolling on 2022-04-29.
+//
+
+import SwiftUI
+
+@main
+struct AsyncPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
